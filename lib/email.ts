43babfc1cb5d15c11420a93decf1e699e140bbdb
Paul Ernest Kachule <58757588+Admin368@users.noreply.github.com>
2025-04-11 08:37:59 +0800

    email fox 5

--- lib/email.ts
+++ lib/email.ts
@@ -36,18 +36,14 @@ export async function sendMagicLink(
   name: string
 ) {
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
-  const magicLinkUrl = `https://${baseUrl}/${
+  const magicLinkUrl = `${baseUrl}/${
     isOrganizer ? "organizer" : "invitation"
   }/verify?token=${token}`;
 
   const mailOptions = {
-    // from: `"Layla & Kondwani" <${
-    //   process.env.SMTP_FROM || process.env.SMTP_USER
-    // }>`,
-    from: {
-      name: "Layla & Kondwani",
-      address: process.env.SMTP_FROM || process.env.SMTP_USER,
-    },
+    from: `"Layla & Kondwani" <${
+      process.env.SMTP_FROM || process.env.SMTP_USER
+    }>`,
     to: email,
     subject: isOrganizer
       ? "Organizer Access Link"
@@ -79,72 +75,42 @@ export async function sendMagicLink(
         </html>
       `
       : `
-      <body style="background: #000;">
-  <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: #000; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: white;">
-        Sign in to <strong>Layla & Kondwani</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="gold"><a href="${magicLinkUrl}"
-                target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: white; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid gold; display: inline-block; font-weight: bold;">Sign
-                in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: white;">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-      `,
-    // : `
-    // <!DOCTYPE html>
-    // <body>
-    //   <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; background-color: #fff; padding: 20px; border: 1px solid #e0e0e0;">
-    //     <div style="text-align: center; margin-bottom: 20px;">
-    //       <h1 style="color: #D4AF37; margin-bottom: 5px;">You're Cordially Invited</h1>
-    //       <h2 style="margin-top: 0;">to a Special Celebration</h2>
-    //       <h3 style="margin-top: 15px;">Honoring</h3>
-    //       <h2 style="color: #D4AF37;">Layla & Kondwani</h2>
-    //     </div>
+    <!DOCTYPE html>
+    <body>
+      <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; background-color: #fff; padding: 20px; border: 1px solid #e0e0e0;">
+        <div style="text-align: center; margin-bottom: 20px;">
+          <h1 style="color: #D4AF37; margin-bottom: 5px;">You're Cordially Invited</h1>
+          <h2 style="margin-top: 0;">to a Special Celebration</h2>
+          <h3 style="margin-top: 15px;">Honoring</h3>
+          <h2 style="color: #D4AF37;">Layla & Kondwani</h2>
+        </div>
 
-    //     <div style="margin: 30px 0; text-align: center;">
-    //       <h4 style="color: #D4AF37;">Dear ${name},</h4>
-    //       <p>You have been invited to the engagement celebration for Layla and Kondwani.</p>
-    //       <p>Please click the button below to view all the details and RSVP.</p>
-    //       <p>This is a surprise proposal for Layla, please do not share this link or discuss the date and time with the bride to be as she is expecting this event to happen on a different date.</p>
-    //     </div>
-
-    //     <div style="text-align: center; margin: 40px 0 20px;">
-    //       <a href="${magicLinkUrl}" style="display: inline-block; padding: 12px 25px; background-color: #D4AF37; color: #fff; text-decoration: none; border-radius: 4px; font-weight: bold; margin: 15px 0;">View Details & RSVP</a>
-    //       <p style="margin-top: 10px; font-size: 0.8em; color: #666;">This link will expire after use.</p>
-    //     </div>
-    //   </div>
-    // </body>
-    // </html>
-    // `,
+        <div style="margin: 30px 0; text-align: center;">
+          <h4 style="color: #D4AF37;">Dear ${name},</h4>
+          <p>You have been invited to the engagement celebration for Layla and Kondwani.</p>
+          <p>Please click the button below to view all the details and RSVP.</p>
+          <p>This is a surprise proposal for Layla, please do not share this link or discuss the date and time with the bride to be as she is expecting this event to happen on a different date.</p>
+        </div>
+        <p>if you cannot click the link copy and paste it into your browser</p>
+        <p>${magicLinkUrl}</p>
+        <div style="text-align: center; margin: 40px 0 20px;">
+          <a href="${magicLinkUrl}" style="display: inline-block; padding: 12px 25px; background-color: #D4AF37; color: #fff; text-decoration: none; border-radius: 4px; font-weight: bold; margin: 15px 0;">View Details & RSVP</a>
+          <p style="margin-top: 10px; font-size: 0.8em; color: #666;">This link will expire after use.</p>
+        </div>
+      </div>
+    </body>
+    </html>
+    `,
   };
 
   // Return the magic link URL in preview mode
   if (isPreviewEnvironment) {
-    await transporter.sendMail(mailOptions as any);
+    await transporter.sendMail(mailOptions);
     return { magicLinkUrl };
   }
 
   // Actually send the email in production
-  return transporter.sendMail(mailOptions as any);
+  return transporter.sendMail(mailOptions);
 }
 
 export async function notifyOrganizers(message: string, subject: string) {
